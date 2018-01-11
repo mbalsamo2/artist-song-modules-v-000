c51a593fa5d60cc2f9db5b6f3b23bd7500cb8508
Maria Balsamo <mia.balsamo@gmail.com>
2018-01-11 01:00:47 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Artist
   extend Memorable::ClassMethods, Findable::ClassMethods
-  include Paramable::InstanceMethods
+  include Paramable::InstanceMethods, Memorable::InstanceMethods
   attr_accessor :name
   attr_reader :songs
 
