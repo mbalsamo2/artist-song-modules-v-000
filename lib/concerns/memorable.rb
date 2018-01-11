d975efc2f26e98a4eec977384506bfc76601e8c2
Maria Balsamo <mia.balsamo@gmail.com>
2018-01-11 00:10:53 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,10 +1,11 @@
 module Memorable
+  module ClassMethods
+    def reset_all
+      all.clear
+    end
 
-  def reset_all
-    all.clear
-  end
-
-  def count
-    @@artists.count
+    def count
+      @@artists.count
+    end
   end
 end
