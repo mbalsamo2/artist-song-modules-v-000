ba233109dccdc49dfbcbd6c570061ffb5f906f1a
Maria Balsamo <mia.balsamo@gmail.com>
2018-01-11 00:44:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,6 @@ require 'pry'
 
 class Song
   extend Memorable::ClassMethods, Findable::ClassMethods
-  extend Findable::ClassMethods
   include Paramable::InstanceMethods
   attr_accessor :name
   attr_reader :artist
